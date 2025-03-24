a78963b19f2083b4df1790b6dbb271654489812c
Ankush Jaiswal <ankushjaiswal1111@gmail.com>
2025-03-24 22:32:36 +0530

    add env and nodemodule in gitignore

--- server.js
+++ server.js
@@ -6,25 +6,19 @@ const connectDB = require("./db");
 
 const newsRoutes = require("./routes/newsRoutes");
 
-// Initialize App
 const app = express();
 app.use(cors({ credentials: true, origin: true }));
 app.use(express.json());
 
-// Connect Database
 connectDB();
 
-// Serve Static Files (Images)
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-// app.use('/uploads', express.static(path.resolve(__dirname, 'uploads')));
 
-// Routes
 app.use("/api", newsRoutes);
 
 app.get("/", (req, res) => {
   res.send("API is Running...");
 });
 
-// Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
